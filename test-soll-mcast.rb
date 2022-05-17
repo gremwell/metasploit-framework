e4c5dcf4600928cada5475fd023a3916cc52697a
Alexandre Bezroutchko <abb@gremwell.com>
2022-05-17 09:22:38 +0000

    test script for the bug in solicited-node multicast address calculation routine

---
+++ test-soll-mcast.rb
@@ -0,0 +1,30 @@
+def orig_ipv6_soll_mcast_addr6(addr)
+  h = addr.split(':')[-2, 2]
+  m = []
+  m << 'ff'
+  m << (h[0].to_i(16) & 0xff).to_s(16)
+  m << ((h[1].to_i(16) & (0xff << 8)) >> 8).to_s(16)
+  m << (h[1].to_i(16) & 0xff).to_s(16)
+  'ff02::1:' + [m[0,2].join, m[2,2].join].join(':')
+end
+
+def fixed_ipv6_soll_mcast_addr6(addr)
+  h = addr.split(':')[-2, 2]
+  m = []
+  m << 'ff'
+  m << (h[0].to_i(16) & 0xff).to_s(16)
+  m << ((h[1].to_i(16) & (0xff << 8)) >> 8).to_s(16)
+  m << (h[1].to_i(16) & 0xff).to_s(16)
+  'ff02::1:' + [m[0,3].join, m[3,2].join].join(':')
+end
+
+def test(unicast, expected)
+  puts "unicast:  " + unicast
+  puts "orig:     " + orig_ipv6_soll_mcast_addr6(unicast)
+  puts "fixed:    " + fixed_ipv6_soll_mcast_addr6(unicast)
+  puts "expected: " + expected
+end
+
+# from https://en.wikipedia.org/wiki/Solicited-node_multicast_address
+test("fe80::2aa:ff:fe28:9c5a", "ff02::1:ff28:9c5a")
+
